85f0e4f553774675426785fc883afc568516e007
Aidan Mahoney <149884417+aidanmahoney@users.noreply.github.com>
2024-12-04 10:44:52 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
-import streamlit as st
-import yfinance as yf
-import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import streamlit as st
+import yfinance as yf
+import pandas as pd
 import numpy as np
 import datetime as dt
 
